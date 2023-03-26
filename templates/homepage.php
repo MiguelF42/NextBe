443db2ae7c9bab0a4062db466d12ba09739270ee
MiguelF42 <m.ferreira@lprs.fr>
2023-03-26 22:46:13 +0200

    Page d'accueil : Réalisation de la barre de navigation (responsive) Réalisation de l'en-tête Début de la barre de recherche

--- templates/homepage.php
+++ templates/homepage.php
@@ -3,12 +3,47 @@
 $title = 'Homepage';
 
 ob_start(); ?>
-<h1>You are on the Homepage</h1>
-<p>
-    Then you must start your project now!
-    <br>
-    I hope this tools helped you to start
-</p>
+        <header id="home">
+            <div id="hero-txt">
+                <h1>
+                    Let's The World Together!
+                </h1>
+                <p>
+                    Find a flight to travel everywhere you want accross the world
+                </p>
+            </div>
+            <div id="search-bar" class="flex-r flex-r-c">
+                <form action="" method="post">
+                    <label for="departure-airport">Point de départ
+                        <select name="departure-airport" id="departure-airport">
+                        </select>
+                    </label>
+                    <label for="arrival-airport">Destination
+                        <select name="arrival-airport" id="arrival-airport">
+    
+                        </select>
+                    </label>
+                    <label for="departure-date">Date de départ
+                        <input name="departure-date" id="departure-date" type="date" min="">
+                    </label>
+                    <label for="arrival-date">Date de retour
+                        <input name="arrival-date" id="arrival-date" type="date" min="">
+                    </label>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </header>
+        <article>
+            <section class="top-dest container">
+                
+            </section>
+            <section class="travel-tips container">
+
+            </section>
+            <section class="why-choose container">
+
+            </section>
+        </article>
 <?php
 
 $content = ob_get_clean();
--- templates/layout.php
+++ templates/layout.php
@@ -1,13 +1,63 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
-    <title> NextBe | <?= $title ?></title>
+    <link rel="stylesheet" href="./css/main.css?<?= time() ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=swap" rel="stylesheet">
+    <title>NextBe | Accueil</title>
 </head>
 <body>
-    <?= $content ?>
+    <header class="common flex-r">
+        <div class="logo-container flex-r flex-r-c">
+            <div class="logo">
+
+            </div>
+            <span>
+                NextBe
+            </span>
+        </div>
+        <nav id="navbar">
+            <div class="links">
+                <ul>
+                    <li><a href="#">A propos</a></li>
+                    <li><a href="#">Vol</a></li>
+                    <li><a href="#">Companie</a></li>
+                    <li><a href="#">Destination</a></li>
+                    <li><a href="#">Modalités</a></li>
+                </ul>
+            </div>
+            <div class="account">
+                <a href="#">
+                    <button>Login</button>
+                </a>
+            </div>
+        </nav>
+        <button id="menu-toggle" class="menu-toggle"> 
+            <span class="burger-menu-box">
+                <span class="burger-menu-inner">
+                    
+                </span>
+            </span>
+        </button>
+    </header>
+    <main>
+        <?= $content ?>
+    </main>
+    <footer>
+        
+    </footer>
+    <script>
+        let nav = document.getElementById('navbar');
+        let btn = document.getElementById('menu-toggle');
+
+        btn.onclick = () => {
+            if(nav.classList.contains('active')) nav.classList.remove('active');
+            else nav.classList.add('active');
+        }
+    </script>
 </body>
 </html>
